4d55c9d25b3313980f20d7e7cb6bdcc6793d8800
Khalid Rehioui <rehioui@gmail.com>
2018-02-23 06:18:47 +0100

    Create index.js and the lists

---
+++ index.js
@@ -0,0 +1,9 @@
+'use strict';
+const path = require('path');
+
+module.exports = {
+    adjectives  : path.join(__dirname, 'lists/adjectives.txt'),
+    adverbs     : path.join(__dirname, 'lists/adverbs.txt'),
+    nouns       : path.join(__dirname, 'lists/nouns.txt'),
+    verbs       : path.join(__dirname, 'lists/verbs.txt')
+};
